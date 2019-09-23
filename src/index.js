09aa759b37aec5f4e3977eff371cc0f92e506d25
Nikolay <n.smirnov.kz@gmail.com>
2019-09-23 19:14:50 +0600

    submit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    var array = [];
+    var brackets = {};
+
+    bracketsConfig.filter((x) =>  {
+		brackets[x[0]] = x[1]
+	})
+    
+    for (let i = 0; i < str.length; i++) {
+        let bracket = str.charAt(i);
+        if (brackets[bracket] === bracket && array[array.length - 1] === bracket) {
+           array.pop();
+        } else if (brackets[bracket]) {
+            array.push(bracket);
+        } else if (bracket != brackets[array.pop()]) {            
+            return false;
+        }
+    }   
+	
+    return array.length === 0;
 }
